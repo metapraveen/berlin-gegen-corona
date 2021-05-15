54eda966262cc3ea10d4d37b1a69dc43f6b73d7c
Praveen <pdnsjce@gmail.com>
2021-05-15 15:47:51 +0200

    option to book mark a doctor

--- src/ClinicCard.tsx
+++ src/ClinicCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from "wouter";
 import copyToClipboard from "./copyToClipboard";
 import { Clinic, keyToHeaderMap } from "./data";
+import { useState } from "react";
 
 const keysOrder: (keyof Clinic)[] = [
   "key_0",
@@ -27,8 +28,37 @@ export default function ClinicCard({
   clinic: Clinic;
   show?: boolean;
 }) {
+  const [bookMarked, setBookMarked] = useState<boolean>(
+    localStorage.getItem(clinic.id!) === "true"
+  );
+  const toggleBookmark = () => {
+    if (bookMarked) {
+      localStorage.removeItem(clinic.id!);
+      setBookMarked(false);
+    } else {
+      localStorage.setItem(clinic.id!, "true");
+      setBookMarked(true);
+    }
+  };
+
   return (
     <div className=" p-3 bg-white rounded-lg w-full subpixel-antialiased shadow-md">
+      <div className="float-right cursor-pointer" onClick={toggleBookmark}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill={bookMarked ? "fill" : "none"}
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+          />
+        </svg>
+      </div>
       {keysOrder.map((key) => {
         if (key === "key_1" || key === "id") return null;
 
